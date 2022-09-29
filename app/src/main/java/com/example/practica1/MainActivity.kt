777feab5c3d5b4fdb31725db3e84a07a951bc35b
alumon574 <alu10522574@ieselcaminas.org>
2022-09-29 16:56:48 +0200

    Ejercicio 4 hecho

--- app/src/main/java/com/example/practica1/MainActivity.kt
+++ app/src/main/java/com/example/practica1/MainActivity.kt
@@ -1,11 +1,22 @@
 package com.example.practica1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val jugador = findViewById(R.id.button2) as Button
+
+        jugador.setOnClickListener { (lanzarNewPlayer() ) }
+    }
+
+    fun lanzarNewPlayer() {
+        val i = Intent (this, NewPlayer::class.java)
+        startActivity(i)
     }
 }
